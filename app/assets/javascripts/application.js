7b2b98b03ec291c97c7031ed8ec383dee7f68819
Redfact <tantelyrandria@ymail.com>
2020-08-10 15:22:01 +0200

    add basic pwa configd

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,32 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+
+window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('ServiceWorker registered: ', registration);
+        
+      
+      var serviceWorker;
+      if (registration.installing) {
+        serviceWorker = registration.installing;
+        console.log('Service worker installing.');
+      } else if (registration.waiting) {
+        serviceWorker = registration.waiting;
+        console.log('Service worker installed & waiting.');
+      } else if (registration.active) {
+        serviceWorker = registration.active;
+        console.log('Service worker active.');
+      }
+    window.Notification.requestPermission().then(permission => {    
+        if(permission !== 'granted'){
+          throw new Error('Permission not granted for Notification');
+        }
+    }); 
+    
+    }).catch(registrationError => {
+      console.log('Service worker registration failed: ', registrationError);
+    });
+    
+  });
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  #serfiveworker configs
+  get '/service-worker.js' => "service_worker#service_worker"
+  get '/manifest.json' => "service_worker#manifest"
+
   devise_for :users
   root to: 'ala_une#show'
   get 'accueil',to: 'ala_une#show'
