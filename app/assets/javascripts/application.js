89ff1b820179dfb31dde10d70eb36685f8f044d2
Redfact <tantelyrandria@ymail.com>
2020-08-11 15:15:37 +0200

    update front layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,7 +24,6 @@ window.addEventListener('load', function(){
     navigator.serviceWorker.register('/service-worker.js').then(registration => {
       console.log('ServiceWorker registered: ', registration);
         
-    
       var serviceWorker;
       if (registration.installing) {
         serviceWorker = registration.installing;
